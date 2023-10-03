b676623c5c86ea71d51d9469454945f10191e1b7
Mathieu Roy <mathroy0310'@gmail.com>
2023-10-03 19:56:23 -0400

    fix strchr

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -1,27 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                     ██   ██ ██████         */
-/*   ft_strchr.c                                       ██   ██      ██        */
-/*                                                     ███████  █████         */
-/*   By: maroy <maroy@student.42.qc>                        ██ ██             */
-/*                                                          ██ ███████.qc     */
-/*   Created: 2022/10/18 13:41:06 by maroy                                    */
-/*   Updated: 2023/09/02 19:03:08 by maroy            >(.)__ <(.)__ =(.)__    */
-/*                                                     (___/  (___/  (___/    */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maroy <maroy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 13:41:06 by maroy             #+#    #+#             */
+/*   Updated: 2023/10/03 19:55:37 by maroy            ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libft.h"
 
-char	*ft_strchr(char *save, int c)
+char	*ft_strchr(const char *str, int c)
 {
+	char	*s;
 	size_t	i;
 
+	s = (char *)str;
 	i = 0;
-	while (save[i] != c)
+	while (i <= ft_strlen(str))
 	{
-		if (save[i] == '\0')
-			return (NULL);
+		if (s[i] == (char)c)
+			return (s + i);
 		i++;
 	}
-	return (save);
+	return (0);
 }
