b146ab332c84218e981d0e4167aa2b6551f00fb6
Mathieu Roy <maroy@c3r12p4.42quebec.com>
2023-12-05 13:34:30 -0500

    debugprint

--- inc/ft_colors.h
+++ inc/ft_colors.h
@@ -6,7 +6,7 @@
 /*   By: maroy <maroy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 12:24:49 by maroy             #+#    #+#             */
-/*   Updated: 2023/12/05 13:06:49 by maroy            ###   ########.fr       */
+/*   Updated: 2023/12/05 13:34:11 by maroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,78 +18,78 @@
  */
 
 //Regular text
-#define FT_BLK "\033[0;30m"
-#define FT_RED "\033[0;31m"
-#define FT_GRN "\033[0;32m"
-#define FT_YEL "\033[0;33m"
-#define FT_BLU "\033[0;34m"
-#define FT_MAG "\033[0;35m"
-#define FT_CYN "\033[0;36m"
-#define FT_WHT "\033[0;37m"
+#define FT_BLK "\001\033[0;30m\002"
+#define FT_RED "\001\033[0;31m\002"
+#define FT_GRN "\001\033[0;32m\002"
+#define FT_YEL "\001\033[0;33m\002"
+#define FT_BLU "\001\033[0;34m\002"
+#define FT_MAG "\001\033[0;35m\002"
+#define FT_CYN "\001\033[0;36m\002"
+#define FT_WHT "\001\033[0;37m\002"
 
 //Regular bold text
-#define FT_BBLK "\033[1;30m"
-#define FT_BRED "\033[1;31m"
-#define FT_BGRN "\033[1;32m"
-#define FT_BYEL "\033[1;33m"
-#define FT_BBLU "\033[1;34m"
-#define FT_BMAG "\033[1;35m"
-#define FT_BCYN "\033[1;36m"
-#define FT_BWHT "\033[1;37m"
+#define FT_BBLK "\001\033[1;30m\002"
+#define FT_BRED "\001\033[1;31m\002"
+#define FT_BGRN "\001\033[1;32m\002"
+#define FT_BYEL "\001\033[1;33m\002"
+#define FT_BBLU "\001\033[1;34m\002"
+#define FT_BMAG "\001\033[1;35m\002"
+#define FT_BCYN "\001\033[1;36m\002"
+#define FT_BWHT "\001\033[1;37m\002"
 
 //Regular underline text
-#define FT_UBLK "\033[4;30m"
-#define FT_URED "\033[4;31m"
-#define FT_UGRN "\033[4;32m"
-#define FT_UYEL "\033[4;33m"
-#define FT_UBLU "\033[4;34m"
-#define FT_UMAG "\033[4;35m"
-#define FT_UCYN "\033[4;36m"
-#define FT_UWHT "\033[4;37m"
+#define FT_UBLK "\001\033[4;30m\002"
+#define FT_URED "\001\033[4;31m\002"
+#define FT_UGRN "\001\033[4;32m\002"
+#define FT_UYEL "\001\033[4;33m\002"
+#define FT_UBLU "\001\033[4;34m\002"
+#define FT_UMAG "\001\033[4;35m\002"
+#define FT_UCYN "\001\033[4;36m\002"
+#define FT_UWHT "\001\033[4;37m\002"
 
 //Regular background
-#define FT_BLKB "\033[40m"
-#define FT_REDB "\033[41m"
-#define FT_GRNB "\033[42m"
-#define FT_YELB "\033[43m"
-#define FT_BLUB "\033[44m"
-#define FT_MAGB "\033[45m"
-#define FT_CYNB "\033[46m"
-#define FT_WHTB "\033[47m"
+#define FT_BLKB "\001\033[40m\002"
+#define FT_REDB "\001\033[41m\002"
+#define FT_GRNB "\001\033[42m\002"
+#define FT_YELB "\001\033[43m\002"
+#define FT_BLUB "\001\033[44m\002"
+#define FT_MAGB "\001\033[45m\002"
+#define FT_CYNB "\001\033[46m\002"
+#define FT_WHTB "\001\033[47m\002"
 
 //High intensty background 
-#define FT_BLKHB "\033[0;100m"
-#define FT_REDHB "\033[0;101m"
-#define FT_GRNHB "\033[0;102m"
-#define FT_YELHB "\033[0;103m"
-#define FT_BLUHB "\033[0;104m"
-#define FT_MAGHB "\033[0;105m"
-#define FT_CYNHB "\033[0;106m"
-#define FT_WHTHB "\033[0;107m"
+#define FT_BLKHB "\001\033[0;100m\002"
+#define FT_REDHB "\001\033[0;101m\002"
+#define FT_GRNHB "\001\033[0;102m\002"
+#define FT_YELHB "\001\033[0;103m\002"
+#define FT_BLUHB "\001\033[0;104m\002"
+#define FT_MAGHB "\001\033[0;105m\002"
+#define FT_CYNHB "\001\033[0;106m\002"
+#define FT_WHTHB "\001\033[0;107m\002"
 
 //High intensty text
-#define FT_HBLK "\033[0;90m"
-#define FT_HRED "\033[0;91m"
-#define FT_HGRN "\033[0;92m"
-#define FT_HYEL "\033[0;93m"
-#define FT_HBLU "\033[0;94m"
-#define FT_HMAG "\033[0;95m"
-#define FT_HCYN "\033[0;96m"
-#define FT_HWHT "\033[0;97m"
+#define FT_HBLK "\001\033[0;90m\002"
+#define FT_HRED "\001\033[0;91m\002"
+#define FT_HGRN "\001\033[0;92m\002"
+#define FT_HYEL "\001\033[0;93m\002"
+#define FT_HBLU "\001\033[0;94m\002"
+#define FT_HMAG "\001\033[0;95m\002"
+#define FT_HCYN "\001\033[0;96m\002"
+#define FT_HWHT "\001\033[0;97m\002"
 
 //Bold high intensity text
-#define FT_BHBLK "\033[1;90m"
-#define FT_BHRED "\033[1;91m"
-#define FT_BHGRN "\033[1;92m"
-#define FT_BHYEL "\033[1;93m"
-#define FT_BHBLU "\033[1;94m"
-#define FT_BHMAG "\033[1;95m"
-#define FT_BHCYN "\033[1;96m"
-#define FT_BHWHT "\033[1;97m"
+#define FT_BHBLK "\001\033[1;90m\002"
+#define FT_BHRED "\001\033[1;91m\002"
+#define FT_BHGRN "\001\033[1;92m\002"
+#define FT_BHYEL "\001\033[1;93m\002"
+#define FT_BHBLU "\001\033[1;94m\002"
+#define FT_BHMAG "\001\033[1;95m\002"
+#define FT_BHCYN "\001\033[1;96m\002"
+#define FT_BHWHT "\001\033[1;97m\002"
 
 //Reset
-#define FT_reset "\033[0m"
-#define FT_CRESET "\033[0m"
-#define FT_COLOR_RESET "\033[0m"
+#define FT_reset "\001\033[0m\002"
+#define FT_CRESET "\001\033[0m\002"
+#define FT_COLOR_RESET "\001\033[0m\002"
 
 #endif
